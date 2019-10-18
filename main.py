37ea4af0c82e9209b29dc8a3f41093496c6fb4ca
Your Name <you@example.com>
2019-10-18 12:01:46 +0530

    Google

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import webapp2
 class arun(webapp2.RequestHandler):
     def get(self):
         self.response.headers['Content-Type'] = 'text/plain'
-        self.response.write('Hello All. Cloud Computing weclomes you')
+        self.response.write('Aloha,Namaste kaisa kay howdy')
 
 
 app = webapp2.WSGIApplication([
